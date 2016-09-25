6d9d683177cbab5512fb30b5c57c6cf44637e9e6
jd2rogers2 <jd2rogers2@gmail.com>
2016-09-25 14:22:29 -0400

    donzo

--- js/jukebox.js
+++ js/jukebox.js
@@ -10,35 +10,43 @@ var artists = Object.keys(jukeboxSongs);
 var theArtist = artists[Math.floor(Math.random()) * artists.length];
 var songToPlay = jukeboxSongs[theArtist];
 
+function menuMessage() {
+  return $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
+}
+
+function playMessage() {
+  return $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
+  return "now playing " + songToPlay + " by " + theArtist;
+}
+
+function pauseMessage() {
+  return $('#songPaused').html(songToPlay + " is paused");
+}
+
+function showMessage() {
+  var songs = "";
+  for (var key in jukeboxSongs) {
+    songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
+  }
+  return $('#showSongs').html(songs);
+}
+
 function randomNumber() {
   return Math.floor(Math.random());
 }
 
 function menu() {
-  $('#menu').on('click', function(e) {
-    $('#menuOptions').html("You can play a song, pause a song, or view all the songs");
-  });
+  $('#menu').on('click', menuMessage);
 }
 
 function play() {
-  $('#play').on('click', function(e) {
-    $('#songPlaying').html("now playing " + songToPlay + " by " + theArtist);
-    return "now playing " + songToPlay + " by " + theArtist;
-  });
+  $('#play').on('click', playMessage);
 }
 
 function pause() {
-  $('#pause').on('click', function(e) {
-    $('#songPaused').html(songToPlay + " is paused");
-  });
+  $('#pause').on('click', pauseMessage);
 }
 
 function show() {
-  $('#show').on('click', function(e) {
-    var songs = "";
-    for (var key in jukeboxSongs) {
-      songs += jukeboxSongs[key] + " by " + key +  " is available to play. "
-    }
-    $('#showSongs').html(songs);
-  });
+  $('#show').on('click', showMessage);
 }
